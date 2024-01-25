91420daa129a74c64a8a6f252d8352cac6ee5132
Mushtaq <83019141+mush902@users.noreply.github.com>
2024-01-25 14:45:28 -0500

    Update p2_func.c
    Add contri info

--- p2_func.c
+++ p2_func.c
@@ -1,10 +1,10 @@
+//Group info:
+//mshaikh2 Mushtaq Ahmed Shaikh
+//ahgada Amay Gada
+
 #include <math.h>
-
-
 //returns the function y(x) = fn
 double fn(double x)
 {
-          //return x*x;
 	  return cos(x);
-          //  return x;
 }
